37ddaaa5ef84de4aace62cdb42be5a616959b566
KastytisMarc <kastytis.marcinkevicius@gmail.com>
2022-09-29 12:14:43 +0300

    task 5 is done

--- Uzduotis_5/main.py
+++ Uzduotis_5/main.py
@@ -2,6 +2,12 @@
 
 # Kitų failų ir žemiau esančio kodo nekeiskite
 
+import modules.num.ints as integers
+from modules.mathematics.addition import add
+from modules.mathematics.division import divide
+from modules.mathematics.multiplication import multiply
+from modules.mathematics.subtraction import subtract
+
 a = add(integers.one, integers.four)
 b = divide(integers.four, integers.two)
 c = subtract(integers.three, integers.two)
